c587c8a601339b738bcc9fcfed02eec168b4295e
JayZenith <jayzenith248@gmail.com>
2024-07-27 19:48:53 -0700

    make it better

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ app.use(express.json());
 
 app.get("/*", function(req,res){
   res.sendFile(
-    path.join(__dirname, "../client/index.html"),
+    path.join(__dirname, "../index.html"),
     function(err){
       if(err){
         res.status(500).send(err);
