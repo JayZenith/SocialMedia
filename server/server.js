4ee81b2ae3fd5f6bafdc34709bd3f92f247ff3f6
JayZenith <jayzenith248@gmail.com>
2024-08-01 15:54:31 -0700

    test

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mysql = require("mysql");
 const cors = require("cors");
 require("dotenv").config();
-config = require("../db.config")
+//config = require("../db.config")
 
 const path = require("path")
 const _dirname = path.dirname("");
